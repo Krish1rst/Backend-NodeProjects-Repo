a6a364f39225958aced2726a846cff11b7c97dd1
krishna <Krishnayadav48065@gmail.com>
2024-02-23 23:41:40 +0530

    data population done+Query parameters on the way

--- Store-Api/controllers/products.js
+++ Store-Api/controllers/products.js
@@ -1,10 +1,28 @@
+const Product=require('../models/product')
+
+
 const getAllProductsStatic=async(req,res)=>{
-    throw new Error('testing async errors')
-    res.status(200).json({msg:'products testing route'})
+    const search='ab'
+    const products =await Product.find({
+        name:{$regex:search,$options:'i'}
+    })
+    res.status(200).json({products,nbHits:products.length})
 }
 
 const getAllProducts=async(req,res)=>{
-    res.status(200).json({msg:'products route from here'})
+    const {featured, company,name}=req.query
+    const queryObject={}
+    if(featured){
+        queryObject.featured=featured==='true'?true:false
+    }
+    if(company){
+        queryObject.company=company
+    }
+    if(name){
+        queryObject.name={$regex:name,$options:'i'}
+    }
+    const products=await Product.find(queryObject);
+    res.status(200).json({products,nbHits:products.length})
 }
 
 module.exports={getAllProducts,getAllProductsStatic}
--- Store-Api/models/product.js
+++ Store-Api/models/product.js
@@ -1,33 +1,33 @@
-const mongoose=require('mongoose')
-const productSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'product name must be provided']
-    },
-    price:{
-        type:String,
-        required:[true,'product price must be provided']
-    },
-    featured:{
-        type:Boolean,
-        default:false
-    },
-    rating:{
-        type:Number,
-        default:4.5,
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now(),
-    },
-    company:{
-        type:String,
-        enum:{
-            value:['ikea','liddy','caressa','marcos'],
-            message:'{VALUE} is not supported'
-        },
-       // enum:['ikea','liddy','caressa','marcos']
-    }
-})
+const mongoose = require('mongoose')
 
-module.exports=mongoose.model('Produts ', productSchema)
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'product name must be provided'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'product price must be provided'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
+      message: '{VALUE} is not supported',
+    },
+    // enum: ['ikea', 'liddy', 'caressa', 'marcos'],
+  },
+})
+module.exports = mongoose.model('Product', productSchema)
