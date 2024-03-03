40b35038bec93878ceb17bb81242717fe4842fa7
krishna <Krishnayadav48065@gmail.com>
2024-03-03 11:07:13 +0530

    Crud operatons done

--- Jobs API/middleware/authentication.js
+++ Jobs API/middleware/authentication.js
@@ -5,17 +5,15 @@ const { UnauthenticatedError } = require('../errors');
 const auth = async (req, res, next) => {
   // check header
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     throw new UnauthenticatedError('Authentication invalid');
   }
   const token = authHeader.split(' ')[1];
 
-  
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     // attach the user to the job routes
-    const testUser = payload.userId === 'jwtSecret';
-    req.user = { userId: payload.userId, testUser };
+    req.user = { userId: payload.userId, name:payload.name };
     next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid');
